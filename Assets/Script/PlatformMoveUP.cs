fa2ccfa1d6dda17f5850b1f864a5cb77943dc6ec
LAPTOP-3MCADDQ7\Vianney Bartnig <vianney.bartnig@lecole-ldlc.com>
2020-11-26 16:21:43 +0100

    More platforms

---
+++ Assets/Script/PlatformMoveUP.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMoveUP : MonoBehaviour
+{
+    Rigidbody rigidbody;
+    bool flag;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        flag = true;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 currentPosition = transform.position;
+        Vector3 direction;
+        if (flag == true)
+        {
+            direction = new Vector3(0f, 1f, 0f);
+            if (currentPosition.y >= 5.0f)
+            {
+                flag = false;
+            }
+        }
+        else
+        {
+            direction = new Vector3(0f, -1f, 0f);
+            if (currentPosition.y <= 0.5f)
+            {
+                flag = true;
+            }
+        }
+
+        float speed = 2f;
+        Vector3 newPosition = currentPosition + direction * speed * Time.deltaTime;
+
+
+        rigidbody.MovePosition(newPosition);
+    }
+}
