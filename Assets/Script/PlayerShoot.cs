786237887be0e28eddc35f0c6d9da70423101b68
LAPTOP-3MCADDQ7\Vianney Bartnig <vianney.bartnig@lecole-ldlc.com>
2020-11-26 13:19:24 +0100

    Player can now shoot projectiles

---
+++ Assets/Script/PlayerShoot.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    [SerializeField]
+    GameObject Projectile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Pressed primary button.");
+            Vector3 position = transform.position + Vector3.up * 0.5f + transform.forward * 0.5f;
+
+            Instantiate(Projectile, position, transform.rotation);
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log("Pressed secondary button.");
+        }
+        if (Input.GetMouseButtonDown(2))
+        {
+            Debug.Log("Pressed middle click.");
+        }
+            
+    }
+}
--- Assets/Script/ProjectileMove.cs
+++ Assets/Script/ProjectileMove.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class ProjectileMove : MonoBehaviour
 {
+
+    Rigidbody rigidbody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Vector3 currentPosition = transform.position;
+        Vector3 direction = transform.forward;
+        float speed = 2f;
+        Vector3 newPosition = currentPosition + direction * speed * Time.deltaTime;
+
+
+        rigidbody.MovePosition(newPosition);
     }
 }
