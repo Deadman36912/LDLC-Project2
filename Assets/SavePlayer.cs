3399bfe9d1c61e68544514a1a90286cca155cc19
LAPTOP-3MCADDQ7\Vianney Bartnig <vianney.bartnig@lecole-ldlc.com>
2020-11-26 11:43:40 +0100

    Transfert de Assets, ProjectSettings, Packages.

---
+++ Assets/SavePlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SavePlayer : MonoBehaviour
+{
+    Vector3 SavedPosition;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SavedPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < SavedPosition.y - 2)
+        {
+            transform.position = SavedPosition;
+        }
+        
+    }
+}
