0b1a02833c6d0ad87ed11bed198aa538dad31b12
LAPTOP-3MCADDQ7\Vianney Bartnig <vianney.bartnig@lecole-ldlc.com>
2020-11-30 12:56:13 +0100

    Intégration de la Cam first person + changement de point de vue à la third person

---
+++ Assets/Script/ActivePlayer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivePlayer : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject Player;
+    [SerializeField]
+    GameObject CMCamera;
+    [SerializeField]
+    GameObject MainCamera;
+    [SerializeField]
+    GameObject FirstPerson;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(Player.activeSelf == false)
+        {
+            Vector3 SavedPosition = transform.position;
+            Player.SetActive(true);
+            CMCamera.SetActive(true);
+            MainCamera.SetActive(true);
+            Player.transform.position = SavedPosition;
+            FirstPerson.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
